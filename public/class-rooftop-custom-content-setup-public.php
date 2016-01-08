ccd7a669ec636eafc7bb243c6a1e2e25d4441675
Ed Jones <ed@errorstudio.co.uk>
2016-01-08 20:46:48 +0000

    Allow excerpt on custom content types

--- public/class-rooftop-custom-content-setup-public.php
+++ public/class-rooftop-custom-content-setup-public.php
@@ -145,7 +145,7 @@ class Rooftop_Custom_Content_Setup_Public {
             'description' => "A $type",
             'public' => true,
             'supports' => array(
-                'title', 'editor'
+                'title', 'editor', 'excerpt'
             ),
             'show_ui' => true,
             'menu_position' => 20,
