933ba3a306c86eae25d88941fc8c1f20a765bf27
Paul Hendrick <paul@errorstudio.co.uk>
2017-06-15 10:12:53 +0100

    allow ordering (page attributes) and wp custom fields to be added to custom post types

--- public/class-rooftop-custom-content-setup-public.php
+++ public/class-rooftop-custom-content-setup-public.php
@@ -145,7 +145,7 @@ class Rooftop_Custom_Content_Setup_Public {
             'description' => "A $type",
             'public' => true,
             'supports' => array(
-                'title', 'editor', 'excerpt', 'thumbnail', 'revisions'
+                'title', 'editor', 'excerpt', 'thumbnail', 'revisions', 'page-attributes', 'custom-fields'
             ),
             'show_ui' => true,
             'menu_position' => 20,
